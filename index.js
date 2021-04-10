a3b36b16c6a0c6ecf48e5420308bd44b9557622c
whoant <vovanhoangtuan4.2@gmail.com>
2021-04-10 23:59:59 +0700

    Add method uploadFile

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const {v4 : uuidv4 } = require('uuid');
+require('dotenv').config();
+const { v4: uuidv4 } = require('uuid');
+const readLineSync = require('readline-sync');
+
 const fs = require('fs');
 
 const Facebook = require('./module');
+let rq = require('request-promise');
 
 
 const getFbdtsg = (cookie) => {
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const api = new Facebook();
         api.setCookie(cookie);
-        
+
         try {
             const res = await api.run({
                 path: '/me'
@@ -28,12 +32,21 @@ const getFbdtsg = (cookie) => {
     });
 };
 
+const postGroup = (paramFb, infoFb) => {
+    return new Promise(async (resolve, reject) => {
+        let attachments = [];
 
-
-const postGroup = (paramFb, idGroup, text) => {
-    return new Promise(async(resolve, reject) => {
-        const {userId, fbDtsg, cookie} = paramFb;
-
+        const { userId, fbDtsg, cookie } = paramFb;
+        const {idGroup, text, listIdPhoto} = infoFb;
+        if (listIdPhoto != null) {
+            attachments = listIdPhoto.map(id => {
+                return {
+                    photo: {
+                        id
+                    }
+                }
+            });
+        }
         const api = new Facebook();
         api.setCookie(cookie);
         const variables = {
@@ -42,7 +55,7 @@ const postGroup = (paramFb, idGroup, text) => {
                     "composer_session_id": uuidv4()
                 },
                 "source": "WWW",
-                "attachments": [],
+                "attachments": attachments,
                 "message": {
                     "ranges": [],
                     "text": text
@@ -84,7 +97,7 @@ const postGroup = (paramFb, idGroup, text) => {
             "prefetchRecentMediaPhotos": true,
             "UFI2CommentsProvider_commentsKey": "CometGroupDiscussionRootSuccessQuery"
         };
-    
+
         const formData = {
             path: '/api/graphql/',
             form: {
@@ -106,12 +119,58 @@ const postGroup = (paramFb, idGroup, text) => {
 
 };
 
-(async() => {
-    try {
-        const COOKIE = '';
-        const paramQuery = await getFbdtsg(COOKIE);
+const uploadImage = (paramFb) => {
+    return new Promise(async (resolve, reject) => {
+        const { userId, fbDtsg, cookie } = paramFb;
 
-        postGroup(paramQuery, '465419364679483', "Võ Văn Hoàng Tuân");
+        try {
+            const options = {
+                method: 'POST',
+                uri: `https://upload.facebook.com/ajax/react_composer/attachments/photo/upload?__user=${userId}&__a=1&fb_dtsg=${fbDtsg}`,
+                formData: {
+                    farr: {
+                        value: fs.createReadStream('1.png'),
+                        options: {
+                            filename: 'test.png',
+                            contentType: 'image/png'
+                        }
+                    },
+                    profile_id: userId,
+                    source: 8,
+                    upload_id: 'jsc_c_8v',
+                    waterfallxapp: 'comet'
+                },
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+                    'Cookie': cookie,
+                }
+            };
+            let res = await rq(options);
+            res = JSON.parse(res.substr(9, res.length));
+            resolve(res);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+(async () => {
+    try {
+        const COOKIE = process.env.COOKIE;
+        
+        const paramQuery = await getFbdtsg(COOKIE);
+        
+        const upload = await uploadImage(paramQuery);
+        
+        let idGroup = readLineSync.question('ID Group : ');
+        // let text = readLineSync.question('Content : ')
+        const infoFb = {
+            idGroup,
+            text: 'Võ Văn Hoàng Tuân',
+            listIdPhoto: [upload.payload.photoID]
+        };
+        const res = await postGroup(paramQuery, infoFb);
+        console.log('Success');
     } catch (error) {
         console.log(error);
     }
--- module.js
+++ module.js
@@ -4,11 +4,7 @@ let rq = require('request-promise');
 rq = rq.defaults({
    baseUrl: 'https://www.facebook.com' ,
    headers: {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'vi,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'dnt': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-J500H Build/LMY48B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.126 Mobile Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-J500H Build/LMY48B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.126 Mobile Safari/537.36'
    }
 });
 
